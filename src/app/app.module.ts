324e234a0870aabd3ea9a025012a82d8b0854d10
codefactorymalmhofer <144675066+codefactorymalmhofer@users.noreply.github.com>
2023-10-30 13:49:39 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Sven-ja/ProjectA-Group9 into main""
    This reverts commit 2e0dc13ca571803c5e116df066b9b2ddd29f398d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { CourseComponent } from './course/course.component';
+=======
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+>>>>>>> 0c6a605f9f1d146c3b804cc80691a2a332aeb71e
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     CourseComponent
+=======
+    TechnologiesComponent,
+    TestimonialsComponent
+>>>>>>> 0c6a605f9f1d146c3b804cc80691a2a332aeb71e
   ],
   imports: [
     BrowserModule,
