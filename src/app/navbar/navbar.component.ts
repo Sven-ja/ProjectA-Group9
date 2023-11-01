eab7cdafe7ee4e4ca37cde1974aebc21d6a79d08
Svenja Schulmeister <schulmeister.svenja@gmail.com>
2023-11-01 10:23:24 +0100

    added a margin to the navbar scrolling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,10 +7,21 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  // navigateToSection(sectionId: string): void {
+  //   const element = document.getElementById(sectionId);
+  //   if (element) {
+  //     element.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // }
+
   navigateToSection(sectionId: string): void {
+    // -15 is coming from the CSS where the Navbar gets an height of 15vh; the +1 makes it look a bit nicer
+    const yOffset = (-15 * window.innerHeight / 100) + 1;
     const element = document.getElementById(sectionId);
+
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
+      const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    };
   }
 }
